c06c524cce0ff1aacd62e9a4d34b942d474b5ec1
nrgzgit <mail@nargiz.dev>
2023-09-11 19:10:13 -0400

    WIP

--- index.js
+++ index.js
@@ -29,28 +29,3 @@ window.addEventListener("scroll", checkScrollPosition);
 
 // Footer Date
 date.innerHTML = new Date().getFullYear();
-
-// Highlight Text On Scroll
-// const paragraphToHighlight = document.querySelectorAll(".highlight");
-// function isInViewport(element) {
-//   const distance = element.getBoundingClientRect();
-//   return (
-//     distance.top >= 0 &&
-//     distance.left >= 0 &&
-//     distance.bottom <=
-//       (window.innerHeight || document.documentElement.clientHeight) &&
-//     distance.right <=
-//       (window.innerWidth || document.documentElement.clientWidth)
-//   );
-// }
-// window.addEventListener(
-//   "scroll",
-//   function (event) {
-//     paragraphToHighlight.forEach((element) => {
-//       if (isInViewport(element)) {
-//         element.classList.add("highlighted");
-//       }
-//     });
-//   },
-//   false
-// );
